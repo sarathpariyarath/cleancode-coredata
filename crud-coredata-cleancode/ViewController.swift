9447b3fc05278f2dd43890ea2fe211fe331cdad6
Sarath P <sarath.pariyarath@gmail.com>
2022-12-31 21:34:52 +0530

    Initial Commit

---
+++ crud-coredata-cleancode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  crud-coredata-cleancode
+//
+//  Created by Sarath P on 31/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
